f51c1112baeeab9303d302db6cf82f14de3241fc
sr47ygt <62671296+sr47ygt@users.noreply.github.com>
2020-06-27 15:26:18 +0800

    Update HW6.py

--- HW6.py
+++ HW6.py
@@ -1,5 +1,5 @@
-## 程式
-```
+程式
+
 import cmath
 
 def root2(a,b,c):
@@ -9,10 +9,10 @@ def root2(a,b,c):
 
 print("root of 8x^6+4x+2=", root2(8, 6, 4))
 print("root of 7x^5+3x+1=", root2(7, 5, 3))
-```
-## 結果
-```
+
+結果
+
 PS D:\python> & "C:/Program Files/Python37/python.exe" d:/python/python_class/ai/python/08-scientific/algebra/HW6.py
 root of 8x^6+4x+2= [(-0.375+0.5994789404140899j), (-0.375-0.5994789404140899j)]
 root of 7x^5+3x+1= [(-0.35714285714285715+0.5486532677049005j), (-0.35714285714285715-0.5486532677049005j)]
-```
+
